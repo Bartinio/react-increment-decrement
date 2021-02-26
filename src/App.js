345687a671a6c3991fa11f5eb2bfe58bfd1a07f1
Karen <karenchakhalyan23@gmail.com>
2021-02-26 22:57:50 +0400

    Added some component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Count from './components/Count';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    count: 0
+  }
+  hendleClickPlus = () => {
+    this.setState({count: this.state.count + 1})
+  }
+  hendleClicMinus = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+    render() {
+      return (
+        <div className="App">
+            Hello From React
+            <br />
+            <button onClick={this.hendleClicMinus} className="btn"> - </button>
+            <p className="para">{this.state.count}</p>
+            <button onClick={this.hendleClickPlus} className="btn"> + </button>
+            <hr />
+            <Count />
+        </div>
+      );
+    } 
 }
 
 export default App;
---
+++ src/components/Count.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Count() {
+  return (
+    <div>
+      <h2>This is the Count component</h2>
+    </div>
+  );
+}
+
+export default Count;
